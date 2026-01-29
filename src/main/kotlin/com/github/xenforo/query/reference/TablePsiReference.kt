096e4822cd098f44875c5fa7bb481f324b650300
Chris Deeming <chris@xenforo.com>
2026-01-29 04:21:34 +0000

    Fix Go To Definition using non-deprecated DbDataSource.findElement API
    Replace deprecated DbPsiFacade.findElement() with DbDataSource.findElement()
    to fix navigation regression and future-proof against API removal.
    
    Bump version to 1.1.3

--- src/main/kotlin/com/github/xenforo/query/reference/TablePsiReference.kt
+++ src/main/kotlin/com/github/xenforo/query/reference/TablePsiReference.kt
@@ -1,10 +1,10 @@
 package com.github.xenforo.query.reference
 
 import com.intellij.database.model.DasTable
+import com.intellij.database.util.DbUtil
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
-import com.intellij.sql.symbols.DasPsiWrappingSymbol
 
 class TablePsiReference(
     element: PsiElement,
@@ -12,11 +12,10 @@ class TablePsiReference(
     private val project: Project,
 ) : PsiReferenceBase<PsiElement>(element, true) {
     override fun resolve(): PsiElement? {
-        val symbol = DasPsiWrappingSymbol(targetTable, project)
-        val navElement = symbol.navigationElement
-        if (navElement !== symbol) {
-            return navElement
-        }
-        return null
+        // Find the DbElement via the containing data source (non-deprecated API)
+        return DbUtil.getDataSources(project)
+            .asSequence()
+            .mapNotNull { dataSource -> dataSource.findElement(targetTable) }
+            .firstOrNull()
     }
 }
